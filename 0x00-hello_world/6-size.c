8b8eb339d54d66db457eb8257d82e6e60fa54c02
redatouati <reda.contacte@gmail.com>
2023-09-07 18:06:32 -0700

    another correction of 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,11 @@ float floatType;
 double doubleType;
 char charType;
 
-// sizeof evaluates the size of a variable
+/**sizeof evaluates the size of a variable**/
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
-printf("Size of a float: %zu byt(e)\n", sizeof(floatType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
