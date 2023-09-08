e5f73e171ac2530c4ec18f6d10a8713b9af4a038
redatouati <reda.contacte@gmail.com>
2023-09-07 17:23:48 -0700

    corrected 4-puts.c yet again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * function main - the entry point
+ * @void: no parameters
+ *
+ * description : writes the string Programming is like building a multilingual puzzle to the standard output
+ *
+ * Rturn: 0 
+ */
 int main(void)
 {
 /*initializiing strings*/
