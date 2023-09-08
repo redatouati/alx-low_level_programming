a875c7d19f81bb217e72ab82dc81999578b4f0f1
redatouati <reda.contacte@gmail.com>
2023-09-08 16:29:12 -0700

    finished 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the entry point
+ * @void: no parameters
+ *
+ * description : writes the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
