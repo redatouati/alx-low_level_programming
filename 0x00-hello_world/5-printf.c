8ef939b32719d493a6d29a4cff2d6334536cf512
redatouati <reda.contacte@gmail.com>
2023-09-07 17:39:45 -0700

    finished 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - the entry point
+ * @void: no parameters
+ *
+ * description : writes Programming is like building a multilingual puzzle
+ *
+ * Return: 0
+ */
+int main(void)
+{
+/*initializiing strings*/
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
