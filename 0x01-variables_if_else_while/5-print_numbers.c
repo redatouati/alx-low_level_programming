16dca3f60113a74599354ca5bc200a96b61f248d
redatouati <reda.contacte@gmail.com>
2023-09-08 18:08:08 -0700

    finished 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the entry point
+ * @void: no parameters
+ *
+ * description : writes the base 10 numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (int n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
