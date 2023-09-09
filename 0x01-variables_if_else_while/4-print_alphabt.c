9de44aeb6fa1d0cbbaa441c6f8eadb7cfdfc55ca
redatouati <reda.contacte@gmail.com>
2023-09-08 17:57:15 -0700

    finished 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the entry point
+ * @void: no parameters
+ *
+ * description : writes the alphabet in exept q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
