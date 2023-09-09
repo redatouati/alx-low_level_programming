7605e97b5e7a1e6e2c280bb4e699c95a08e7cccb
redatouati <reda.contacte@gmail.com>
2023-09-08 18:28:39 -0700

    finished 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the entry point
+ * @void: no parameters
+ *
+ * description : writes the base 10 numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + n);
+	putchar('\n');
+	return (0);
+}
