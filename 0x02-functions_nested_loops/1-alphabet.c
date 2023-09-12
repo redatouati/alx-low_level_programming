3e1dbebcecf05a10eec01cccf9bc0e701a184344
redatouati <reda.contacte@gmail.com>
2023-09-12 16:13:51 -0700

    finished 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * no parrams - void
+ *
+ * Description : calls function prints_alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - prints the alphabet
+ * no parrams - void
+ *
+ * Description : prints _putchar
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int n;
+
+	n = 0;
+	for (n = 0; n <= 26; n++)
+		putchar(c[n]);
+}
