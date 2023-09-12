33de5b3389bad313b63deb4094b922afac7469a2
redatouati <reda.contacte@gmail.com>
2023-09-12 15:39:11 -0700

    fixed 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point
  * no parrams - void
  *
- * Description : prints _putchar
+ * Description : calls function prints_putchar
  * Return : always 0
  */
-
 int main(void)
+{
+	print_putchar();
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * print_putchar - prints _putchar
+ * no parrams - void
+ *
+ * Description : prints _putchar
+ * Return 0
+ */
+void print_putchar(void)
 {
 	char c[] = "_putchar";
 	int n;
-	
+
 	n = 0;
 	for (n = 0; n <= 8; n++)
 		putchar(c[n]);
-	putchar('\n');
-	return (0);
 }
