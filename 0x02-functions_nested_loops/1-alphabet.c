548d15ce90ea83e4f584f96f26f81042763caeee
redatouati <reda.contacte@gmail.com>
2023-09-13 03:36:22 -0700

    fixed the _putchar prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * main - entry point
- * no parrams - void
- *
- * Description : calls function prints_alphabet
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - prints the alphabet
  * no parrams - void
  *
@@ -22,10 +9,9 @@ int main(void)
  */
 void print_alphabet(void)
 {
-	char c[] = "abcdefghijklmnopqrstuvwxyz\n";
-	int n;
+	char c;
 
-	n = 0;
-	for (n = 0; n <= 26; n++)
-		putchar(c[n]);
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
