de66e855b0111c61d0ec41a7143e46f6bc517af7
redatouati <reda.contacte@gmail.com>
2023-09-13 04:06:14 -0700

    finished 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _islower - checks lowercase.
+*
+* Return: Always 0.
+*/
+int _islower(int c)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		if (c == chr)
+			return (1);
+	}
+	return (0);
+}
