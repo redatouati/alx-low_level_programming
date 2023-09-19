d9535585bad3487473fbf5f53d624745e28b6287
redatouati <reda.contacte@gmail.com>
2023-09-19 13:57:43 -0700

    finished 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+* _puts - returns the length of a string
+* @str: the string to print
+*
+* Return: int
+*/
+void _puts(char *str)
+{
+	_putchar(str);
+	_putchar("\n");
+}
