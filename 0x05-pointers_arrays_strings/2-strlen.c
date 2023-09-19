eccd7b74df08cfd89805012a01dd1ae1a78b6aac
redatouati <reda.contacte@gmail.com>
2023-09-19 13:35:17 -0700

    corrected 2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,14 @@
 #include <string.h>
 /**
 * _strlen - returns the length of a string
-* @*s: pointer to char, the string
+* @s: the string to return length from
 *
 * Return: int
 */
 int _strlen(char *s)
 {
-	/*int n = 4;*/
 	int length;
-	
+
 	length = strlen(s);
 
 	return (length);
